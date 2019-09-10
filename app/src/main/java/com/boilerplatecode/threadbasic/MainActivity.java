4667dbd31889fef860835e792006340eb13a651c
mladen <ilisevic@gmail.com>
2019-09-10 14:02:52 +0200

    Initial commit

---
+++ app/src/main/java/com/boilerplatecode/threadbasic/MainActivity.java
@@ -0,0 +1,83 @@
+package com.boilerplatecode.threadbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+///TODO nastaviti iz coding in flow videao od 8:00 minute
+public class MainActivity extends AppCompatActivity {
+private TextView tv;
+private Button btnStart, btnStop;
+private int inn=0;
+
+private Handler newHandler = new Handler();
+private volatile boolean stopThread=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv);
+
+
+        btnStart = findViewById(R.id.buttonStart);
+        btnStop = findViewById(R.id.buttonStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startThread();
+            }
+        });
+
+btnStop.setOnClickListener(new View.OnClickListener(){
+    @Override
+    public void onClick(View v) {
+        stopThread();
+    }
+});
+    }
+
+
+public void startThread ()
+{
+
+    stopThread=false;
+    RunnableSolo runnable = new RunnableSolo();
+    new Thread(runnable).start();
+
+
+
+}
+
+public void stopThread()
+{
+    Log.d( "***************", "StopStopStop");
+    stopThread=true;
+
+}
+
+
+    class RunnableSolo implements  Runnable {
+        @Override
+        public void run() {
+
+            for (int i = 1; i < 10; i++) {
+                if (stopThread) return;
+
+                Log.d( "***************", "runnable: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
